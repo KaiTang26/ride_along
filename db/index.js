0f60aa687bf5500393e51e8f66660a392eaa3fe1
vagrant <seb.namek@gmail.com>
2017-11-17 15:42:54 +0000

    postgres working before config

--- db/index.js
+++ db/index.js
@@ -1,3 +1,5 @@
+
+
 const debug = require('debug')('sql')
 const chalk = require('chalk')
 const Sequelize = require('sequelize')
@@ -5,18 +7,21 @@ const pkg = require('../package.json')
 
 const name = process.env.DATABASE_NAME || pkg.name;
 
-const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
+const url = "postgres://sebastian@localhost:5433/ride_along"
 
 console.log(chalk.yellow(`Opening database connection to ${url}${name}`));
 
 // create the database instance
-const db = module.exports = new Sequelize(url, {
+const db = module.exports = new Sequelize('ride_along', 'sebastian', null, {
   logging: debug, // export DEBUG=sql in the environment to get SQL queries
   define: {
     underscored: true,       // use snake_case rather than camelCase column names
     freezeTableName: true,   // don't change table names from the one specified
     timestamps: true,        // automatically include timestamp columns
-  }
+  },
+  host: '/var/run/postgresql',
+  dialect: 'postgres',
+  port: 5433
 })
 
 // pull in our models
