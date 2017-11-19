2565b18652e5ada387a9d711d1c58d522a7cbb2a
Corina Wong <hello@corinawong.com>
2017-11-18 22:20:48 -0500

    Add splash image and logotype

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import logo from './logo.svg';
-import './style.css';
+import splash from './splash.jpg';
+
+const Header = styled.section`
+  height: 600px;
+  background: url(${splash});
+  max-width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-position-y: 55%;
+  color: white;
+  margin-bottom: 4em;
+`;
+const Darken = styled.div`
+  max-width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.66);
+`;
+const SignIn = styled.div`
+  float: right;
+`;
+const Login = styled.button`
+  background: transparent;
+  color: white;
+  font-family: Lato;
+  border: 0;
+  font-size: 1em;
+  padding: 1em;
+`;
+
+const Register = Login.extend`
+`;
 
 class App extends Component {
 
@@ -10,13 +41,28 @@ class App extends Component {
     const { className, ...props } = this.props;
     return (
       <div className={classnames('App', className)} {...props}>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React - Fullstack!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header>
+          <Darken>
+            <SignIn>
+              <Login>
+                Login
+              </Login>
+              <Register>
+                Register
+              </Register>
+            </SignIn>
+            <div id="name">
+              <h1>Ride Along</h1>
+              <h2>Get there <span>>></span> together</h2>
+            </div>
+          </Darken>
+        </Header>
+
+        {/* <FindRide>
+        </FindRide>
+        <PostRide>
+        </PostRide> */}
+
         <Link to='about'><button>Test React Router</button></Link>
         <br />
         <br />
