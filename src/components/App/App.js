2ce17a03543a61334b8c7664bc62b497dd13b7bf
Corina Wong <hello@corinawong.com>
2017-11-20 14:18:29 +0000

    Remove old logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { Link } from 'react-router-dom';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom'
 import FrontHeader from '../Front/FrontHeader.js';
 
 class App extends Component {
