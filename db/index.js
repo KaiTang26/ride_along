a2e09bcdd1360a2c119da118932816da0bdd3f23
Sebastian <seb.namek@gmail.com>
2017-11-21 11:05:44 -0500

    dotenv added

--- db/index.js
+++ db/index.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
+
 const debug = require('debug')('sql')
 const chalk = require('chalk')
 const Sequelize = require('sequelize')
 const pkg = require('../package.json')
 
+
 const name = process.env.DATABASE_NAME || pkg.name;
 
-const url = process.env.DATABASE_URL || `postgres://postgres:StClair@localhost:5432/${name}`;
+const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${url}${name}`));
 
