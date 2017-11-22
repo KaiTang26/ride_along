51b773312acffa4105f31be46f2a11f2f7c13ec1
Sebastian <seb.namek@gmail.com>
2017-11-21 19:51:01 -0500

    started new chat branch

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const app = require('./app');
-
 const PORT = process.env.PORT || 8000;
+
+const server = require ('http').createServer();
+const io = require("socket.io")(server, {
+
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+io.on('connection', function (client){
+  console.log('works');
+})
+
+server.listen(3001);
 
 module.exports=app;
