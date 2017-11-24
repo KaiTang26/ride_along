bdc0644a5d49c751ea13f5679fbe1e382cd048cb
Sebastian <seb.namek@gmail.com>
2017-11-24 17:53:21 -0500

    before merging with dev

--- server/reviews.js
+++ server/reviews.js
@@ -4,13 +4,11 @@ const Review = require('../db/models/review');
 const router = require('express').Router()
 
 router.get('/', function(req, res, next) {
-    Review.findAll({
-            include: [Product]
-        })
-        .then(result => {
-            res.status(200).send(result);
-        })
-        .catch(next);
+  Review.findAll()
+    .then(result => {
+      res.status(200).send(result);
+    })
+    .catch(next);
 });
 
 router.get('/:id', function(req, res, next) {
@@ -23,5 +21,17 @@ router.get('/:id', function(req, res, next) {
         })
         .catch(next);
 });
+
+router.post('/:id', function(req, res, next) {
+  Review.create({
+    review_text: req.body.review_text,
+    rating: req.body.rating,
+    trip_id:req.params.id
+  })
+  .then(review => {
+    console.log(review);
+  })
+  .catch(next);
+})
 
 module.exports = router;
--- src/components/UserProfile/CompletedRides.js
+++ src/components/UserProfile/CompletedRides.js
@@ -4,6 +4,8 @@ import RideReview from './RideReview';
 
 class CompletedRides extends Component {
 
+
+
   render() {
     return (
       <div className="completed-rides">
