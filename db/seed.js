6370fe20e5f36ff1897fef750dc400bc8f8275da
Sebastian <seb.namek@gmail.com>
2017-11-23 20:18:28 -0500

    database restructured

--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,5 @@
 const db = require('../db')
 
-const seedReviews = () => db.Promise.map([
- {rating: 1, review_text: "awful",trip_id:5},
- {rating: 1, review_text: "if you have too much extra money ",trip_id:1},
- {rating: 5, review_text: "the best!",trip_id:2},
- {rating: 2, review_text: "waste of money",trip_id:3},
- {rating: 3, review_text: "can be better",trip_id:4},
- {rating: 3, review_text: "should be better",trip_id:6},
- {rating: 4, review_text: "good price",trip_id:7},
- {rating: 4, review_text: "just like description",trip_id:1}
- ], review => db.model('reviews').create(review));
-
 
 const seedUsers = () => db.Promise.map([
   {first_name: "Ben", last_name:"Folds", email: "ben@folds.com"},
@@ -19,23 +8,34 @@ const seedUsers = () => db.Promise.map([
 
 const seedTrips = () => db.Promise.map([
   {date: new Date(2017, 02, 25), time: `22:47:21 GMT`, start_location: "Toronto", end_location: "Ottawa", passengers: 4},
-  {date: '2017-12-31', time: `09:00:21 GMT`, start_location: "Montreal", end_location: "Hamilton", passengers: 2},
-  {date: '2017-12-22', time: `18:45:21 GMT`, start_location: "Kingston", end_location: "Toronto", passengers: 3},
-  {date: '2017-12-22', time: `18:45:21 GMT`, start_location: "Kingston", end_location: "Montreal", passengers: 2},
-  {date: '2017-12-22', time: `18:45:21 GMT`, start_location: "London", end_location: "Hamiltion", passengers: 4},
-  {date: '2017-12-22', time: `18:45:21 GMT`, start_location: "Hamilton", end_location: "London", passengers: 1},
-  {date: '2017-12-22', time: `18:45:21 GMT`, start_location: "Waterloo", end_location: "Toronto", passengers: 5},
-  {date: '2017-12-22', time: `18:45:21 GMT`, start_location: "Kingston", end_location: "Ottawa", passengers: 1}
+  {date: new Date(2017, 11, 22), time: `09:00:21 GMT`, start_location: "Montreal", end_location: "Hamilton", passengers: 2},
+  {date: new Date(2017, 6, 04), time: `18:45:21 GMT`, start_location: "Kingston", end_location: "Toronto", passengers: 3},
+  {date: new Date(2018, 0, 13), time: `18:45:21 GMT`, start_location: "Kingston", end_location: "Montreal", passengers: 2},
+  {date: new Date(2017, 11, 25), time: `18:45:21 GMT`, start_location: "London", end_location: "Hamiltion", passengers: 4},
+  {date: new Date(2018, 02, 13), time: `18:45:21 GMT`, start_location: "Hamilton", end_location: "London", passengers: 1},
+  {date: new Date(2017, 09, 05), time: `18:45:21 GMT`, start_location: "Waterloo", end_location: "Toronto", passengers: 5},
+  {date: new Date(2017, 07, 13), time: `18:45:21 GMT`, start_location: "Kingston", end_location: "Ottawa", passengers: 1}
 ], trip => db.model('trips').create(trip));
+
+const seedReviews = () => db.Promise.map([
+  {rating: 1, review_text: "Really rude, bad taste in music",trip_id:4},
+  {rating: 1, review_text: "Was late and dropped me off far from agreed point ",trip_id:1},
+  {rating: 5, review_text: "Amazing trip. Nice, funny and on time!",trip_id:2},
+  {rating: 2, review_text: "It was okay. Nothing special but good enough",trip_id:3},
+  {rating: 3, review_text: "Could have been better",trip_id:4},
+  {rating: 3, review_text: "Drove pretty slow",trip_id:3},
+  {rating: 4, review_text: "Nice guy and fair price for the ride",trip_id:1},
+  {rating: 4, review_text: "Had a great time with Bill",trip_id:1}
+], review => db.model('reviews').create(review));
 
 
  db.didSync
    .then(() => db.sync())
-   .then(seedReviews)
-   .then(reviews => console.log(`Seeded ${reviews.length} reviews OK`))
    .then(seedUsers)
    .then(users => console.log(`Seeded ${users.length} users OK`))
    .then(seedTrips)
    .then(trips => console.log(`Seeded ${trips.length} trips OK`))
+   .then(seedReviews)
+   .then(reviews => console.log(`Seeded ${reviews.length} reviews OK`))
    .catch(error => console.error(error))
    .finally(() => db.close())
