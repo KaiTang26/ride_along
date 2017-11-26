2e0bc882013b9d7cccf385bbaf953e50c3f12c8c
Corina Wong <hello@corinawong.com>
2017-11-26 18:38:11 -0500

    Small fix

--- src/components/Rides/Details.js
+++ src/components/Rides/Details.js
@@ -23,27 +23,27 @@ const RideDetailUI = (props) => {
 
   return(
     <div>
-<Menu/>
-  <Container>
+      <Menu/>
+      <Container>
         <Map origin={props.id.origin} destination={props.id.destination}/>
-    <h1>Ride from {props.id.start_location} to {props.id.end_location} </h1>
-    <h2>Leaving {props.id.date} at {props.id.time}</h2>
-    <br></br>
-    <h2>Name: {props.id.driver}</h2>
-    <div>Image placeholder</div>
-    <br></br>
-    <h3>{props.id.driver} is looking to have {props.id.passengers} passengers join the ride.</h3>
-    <br></br>
-    <p>Some text in paragraph form</p>
-    <br></br>
-    <DisplayCondition user={currentUser} statements={props.id.agreements} isDriver={isDriver}/>
-    {isDriver
-      ? <AddCondition tripId={props.id.id}/>
-      : null}
-    
+        <h1>Ride from {props.id.start_location} to {props.id.end_location} </h1>
+        <h2>Leaving {props.id.date} at {props.id.time}</h2>
+        <br></br>
+        <h2>Name: {props.id.driver}</h2>
+        <div>Image placeholder</div>
+        <br></br>
+        <h3>{props.id.driver} is looking to have {props.id.passengers} passengers join the ride.</h3>
+        <br></br>
+        <p>Some text in paragraph form</p>
+        <br></br>
+        <DisplayCondition user={currentUser} statements={props.id.agreements} isDriver={isDriver}/>
+        {isDriver
+          ? <AddCondition tripId={props.id.id}/>
+          : null}
+      
 
-  </Container>
-  </div>
+      </Container>
+    </div>
   )
 }
 
