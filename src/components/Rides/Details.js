6d111c095ec58314f93b6e5b03450b5ed4c99fc6
Sebastian <seb.namek@gmail.com>
2017-11-28 12:01:17 -0500

    image and name now shows up on details component

--- src/components/Rides/Details.js
+++ src/components/Rides/Details.js
@@ -8,17 +8,14 @@ import styled from 'styled-components';
 import gs from '../GlobalStyles.js';
 import Menu from '../Menu';
 
-
 const Container = styled.div`
-  padding-top: 60px;
+  padding-top: 65px;
 `;
 const Left = styled.div`
   float: left;
   width: 40%;
   z-index: 1;
   position: relative;
-  padding: 0px 1.75em 0 1.45em;
-  line-height: 1.5;
 `;
 const Right = styled.div`
   float: right;
@@ -26,32 +23,6 @@ const Right = styled.div`
   position: fixed;
   right: 0;
 
-`;
-
-const H1 = styled.h1`
-  font-weight: 900;
-  font-size: 120%;
-  margin: 2em 0 1.5em !important;
-
-`;
-
-const Label = styled.p`
-  font-size: 75%;
-  // letter-spacing: .5px;
-  font-family: Lato;
-  text-transform: uppercase;
-  margin-bottom: .85em;
-  color: ${gs.golden};
-  display: inline-block;
-  margin-right: .25em;
-  font-weight: bold;
-`;
- const P = styled.p`
-  display: inline-block;
- `;
-
- const Field = styled.div`
-  margin: .5em 0;
 `;
 
 const currentUser = localStorage.getItem("user_id");
@@ -74,36 +45,25 @@ const RideDetailUI = (props) => {
 
   return(
     <div>
-
-    <H1>{props.id.start_location} to {props.id.end_location} </H1>
-    <Field>
-    <Label>Total distance:</Label> <P>{props.id.distance}</P>
-    </Field>
-    <Field>
-    <Label>Total duration:</Label> <P>{props.id.duration}</P>
-    </Field>
-    <Field>
-    <Label>Leaving:</Label> <P>{props.id.date} </P>
-    </Field>
-    <Field>
-    <Label>Time:</Label><P>{props.id.time}</P>
-    </Field>
-    <Field>
-    <Label>Name</Label> <P>{props.id.driver}></P>
-    <div>Image placeholder</div>
-    </Field>
-    <Field>
-    <P>{props.id.driver} NAME is looking to have {props.id.passengers} passengers join the ride.</P>
-    <P>Some text in paragraph form</P>
-    </Field>
-    <Field>
-      <DisplayCondition user={currentUser} statements={props.id.agreements} isDriver={isDriver}/>
+    {console.log(props)}
+    <h1>Ride from {props.id.start_location} to {props.id.end_location} </h1>
+    <h2>Total distance: {props.id.distance}</h2>
+    <h2>Total duration: {props.id.duration}</h2>
+    <h2>Leaving {props.id.date} at {props.id.time}</h2>
+    <br></br>
+    {/* <getDriverName/> */}
+    <h2>{props.driver.first_name}</h2>
+    <img src={`/images/${props.driver.picture}`}/>
+    <br></br>
+    <h3>{props.id.driver} is looking to have {props.id.passengers} passengers join the ride.</h3>
+    <br></br>
+    <p>Some text in paragraph form</p>
+    <br></br>
+      {/* <DisplayCondition user={currentUser} statements={props.id.agreements} isDriver={isDriver}/> */}
       {isDriver
         ? <AddCondition tripId={props.id.id}/>
         : null}
-     </Field>
     </div>
-
   )
 }
 
@@ -111,7 +71,7 @@ class Details extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: "",
+      user: "",
       ride:{driver:0}
     };
 
@@ -120,16 +80,16 @@ class Details extends Component {
       let ride = result.data
       // alert(result.data.driver)
       this.setState({ride})
-      // api.userInfo(result.data.driver)
-      // .then(results => {
-      //   this.setState({
-      //     ride:results
-      //   })
-      // })
+      return result
     })
-    // .then(result => {
-    //
-    // })
+    .then(result => {
+      api.userInfo(1)
+      .then(results => {
+        this.setState({
+          user:results.data
+        })
+      })
+    })
   }
 
 
@@ -141,19 +101,18 @@ class Details extends Component {
         <Menu/>
         <Container>
           <Left>
-            {console.log(this.state)}
           {this.state?
               <div>
-                <RideDetailUI id={this.state.ride} />
+                <RideDetailUI id={this.state.ride} driver={this.state.user} />
                 <ChatContainer id={this.state.ride} />
               </div>
               :"" }
           </Left>
 
           <Right>
-          {/* {this.state?
+          {this.state.ride?
           <RideMap id={this.state.ride} />
-            : ""} */}
+            : ""}
 
           </Right>
         </Container>
