c1c2c512e7d32ab06e124dd679cbb1836067647a
Sebastian <seb.namek@gmail.com>
2017-11-27 20:07:40 -0500

    added an assets folder and display the picture url in posts

--- src/components/Agreement/DisplayCondition.js
+++ src/components/Agreement/DisplayCondition.js
@@ -24,60 +24,61 @@ export default class DisplayCondition extends Component {
   // handleEdit = e => {
   //   // api.editCondition()
   // }
- 
+
   // handleDelete = (trip, id) => {
   //   api.deleteCondition(trip, id);
   //   window.location.reload();
   // }
 
-  
+
 
   render() {
 
 
     return (
       <div>
+        {console.log(this.props)}
         {this.props.statements.map((condition) => (
           <div>
-            <p>{condition.statement}</p> 
+            <p>{condition.statement}</p>
             {this.props.isDriver
               ? <div>
-                <EditCondition 
-                  tripId={condition.trip_id} 
-                  id={condition.id} 
+                <EditCondition
+                  tripId={condition.trip_id}
+                  id={condition.id}
                   statement={condition.statement}/>
 
 
-                {/* <Delete 
+                {/* <Delete
                   onClick={ e => {
                     this.handleDelete(condition.trip_id, condition.id)
                   }
                 }
                 >Delete</Delete> */}
-                <Delete 
-                  tripId={condition.trip_id} 
-                  id={condition.id} 
+                <Delete
+                  tripId={condition.trip_id}
+                  id={condition.id}
                 />
 
               </div>
             :
-            // <CheckAgree user={this.props.user} tripId={condition.trip_id} condition={condition.id} 
+            // <CheckAgree user={this.props.user} tripId={condition.trip_id} condition={condition.id}
             // id={condition.id}/>
 
             <EditEx
-            user={this.props.user} tripId={condition.trip_id} condition={condition.id} 
-            id={condition.id}         
-            
-            // find={api.findPassengerAgreement(condition.trip_id, condition.id, this.props.user)}    
+            user={this.props.user} tripId={condition.trip_id} condition={condition.id}
+            id={condition.id}
+
+            // find={api.findPassengerAgreement(condition.trip_id, condition.id, this.props.user)}
           />
-            // <RadioToggle toggle user={this.props.user} tripId={condition.trip_id} condition={condition.id} 
-            // id={condition.id} /> 
+            // <RadioToggle toggle user={this.props.user} tripId={condition.trip_id} condition={condition.id}
+            // id={condition.id} />
             }
 
 
         </div>
       ))}
-    </div>  
+    </div>
     );
   }
 }
